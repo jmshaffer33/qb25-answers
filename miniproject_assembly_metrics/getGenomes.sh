f3b1c7f90477b53e0049b6a116d8d03acd579419
Jessica Shaffer <jmshaffer33@gmail.com>
2025-09-04 23:57:55 -0400

    Adding comment

--- miniproject_assembly_metrics/getGenomes.sh
+++ miniproject_assembly_metrics/getGenomes.sh
@@ -4,3 +4,5 @@ wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/speci
 wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA248911/caenorhabditis_remanei.PRJNA248911.WBPS19.genomic.fa.gz
 wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA53967/caenorhabditis_remanei.PRJNA53967.WBPS19.genomic.fa.gz
 wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS19/species/caenorhabditis_remanei/PRJNA577507/caenorhabditis_remanei.PRJNA577507.WBPS19.genomic.fa.gz 
+
+#obtaining compressed fasta files for each C. remanei genome, depositing into miniproject_assembly_metrics directory
